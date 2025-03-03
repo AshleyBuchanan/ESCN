c35274830a82d1aaa71295ddaa919443572ebcd9
root <root@RED-BARON1>
2025-03-03 12:18:43 -0800

    Finished writing the initial index.js express server. Opens to a blank screen with 'hello from ESCN' on port 3000.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const { prototype } = require('events');
 const express = require('express');
+const methodOverride = require('method-override');
+const path = require('path');
 const app = express();
+const port = 3000;
+
+app.use(methodOverride('__method'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('hello from ESCN');
+});
+
+app.listen(port, () => {
+    console.log(`listening on port${port}.`);
+});
